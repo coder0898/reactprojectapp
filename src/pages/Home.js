18cf5f2c67e16004019bbf241836753c4d3831fa
Pratik <bakshi.pratikcool@gmail.com>
2023-05-30 17:34:20 +0530

    Home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Pages.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={style.home_container}>
+       <div className={style.home_img}>
+      <i className="fa-brands fa-react"></i>
+      </div>
+      <div className={style.home_desc}>
+         <h2>React Project App</h2>
+         <p>Place to find all react based Web Application project.Starting from small to large clone application.</p>
+         <button>
+         <Link to='/project' style={{textDecoration:"none"}}>See All Project Here!</Link>
+         </button>
+      </div>
+    </div>
   )
 }
 
