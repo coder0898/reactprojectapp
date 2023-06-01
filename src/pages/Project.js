8f56fc40cea185cfcb5d8b13ceba869d3f4f3f60
Pratik <bakshi.pratikcool@gmail.com>
2023-06-01 12:34:10 +0530

    Project page done with responsiveness

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './Pages.module.css';
 
 const Project = () => {
+
+  const [pageNo, setPageNo] = useState(0);
+
+  const projects =['Counter','TempControl','Calc'];
+
+  const pagesDisplay =()=>{
+    if (pageNo === 0) {
+      return projects[pageNo];
+    } else if(pageNo === 1) {
+      return projects[pageNo]; 
+    }else{
+      return projects[pageNo];
+    }
+  }
+
   return (
-    <div>Project</div>
+    <div className={style.project_container}>
+       <div className={style.project_head}>
+          <span>Projects: {pageNo+1}/{projects.length}</span>
+       </div>
+       <div className={style.project_display}>
+         {/* projects comes here */}
+         {pagesDisplay()}
+       </div>
+       <div className={style.project_button_group}>
+       {
+        pageNo === 0? '': <button onClick={()=>setPageNo((currPage)=> currPage - 1 )}>Previous</button>
+       } 
+       {
+        pageNo === projects.length-1? '': <button onClick={()=>setPageNo((currPage)=> currPage + 1 )}>Next</button>
+       }
+       </div>
+     </div>
   )
 }
 
